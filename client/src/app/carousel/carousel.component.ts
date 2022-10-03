291360cae8b61ffda3fa941c55e94fc52aa21bdc
Louka- <simon.haudegond@gmail.com>
2022-10-03 12:00:41 +0200

    carousel finalisation

--- client/src/app/carousel/carousel.component.ts
+++ client/src/app/carousel/carousel.component.ts
@@ -10,19 +10,33 @@ interface carouselImage {
   templateUrl: './carousel.component.html',
   styleUrls: ['./carousel.component.sass']
 })
-export class CarouselComponent implements OnInit {
+export class CarouselComponent {
   @Input() images: carouselImage[] = [];
   @Input() indicators = true;
+  @Input() controls = true;
 
   selectedIndex = 0;
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
-
   selectedImage(index: number) {
     this.selectedIndex = index;
+  }
+
+  onPrevClick(): void {
+    if (this.selectedIndex === 0) {
+      this.selectedIndex = this.images.length - 1;
+    } else {
+      this.selectedIndex--;
+    }
+  }
+
+  onNextClick(): void {
+    if (this.selectedIndex === this.images.length - 1) {
+      this.selectedIndex = 0;
+    } else {
+      this.selectedIndex++;
+    }
   }
 
 }
