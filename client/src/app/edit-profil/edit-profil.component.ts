789e330e540a821a0120602a91a8ce1f5d53ac00
Louka- <simon.haudegond@gmail.com>
2022-10-04 16:23:27 +0200

    get user id from localStorage in edit profil component

--- client/src/app/edit-profil/edit-profil.component.ts
+++ client/src/app/edit-profil/edit-profil.component.ts
@@ -1,16 +1,21 @@
 import { NgForm } from '@angular/forms';
-import { Component, OnInit } from '@angular/core';
-import { Observable, switchMap } from 'rxjs';
+import { Component } from '@angular/core';
+import { map, Observable, switchMap } from 'rxjs';
 import { Profil } from '../models/profil.model';
 import { ProfilService } from '../service/profil.service';
+import { UserService } from '../service/user.service';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-edit-profil',
   templateUrl: './edit-profil.component.html',
   styleUrls: ['./edit-profil.component.sass']
 })
-export class EditProfilComponent implements OnInit {
-  profil$: Observable<Profil> | undefined;
+export class EditProfilComponent {
+  userId = this.auth.getCurrentUserId();
+  profil$: Observable<Profil> = this.userService.getUserById(this.userId).pipe(
+    map(user => user.profil)
+  );
 
   races = [
     { value: "Humain" },
@@ -20,14 +25,12 @@ export class EditProfilComponent implements OnInit {
   ]
   constructor(
     private profilService: ProfilService,
+    private userService: UserService,
+    private auth: AuthService,
   ) { }
 
-  ngOnInit(): void {
-    this.profil$ = this.profilService.getUserProfil(15);
-  }
-
   validate(form: NgForm) {
-    return this.profilService.getUserProfil(15).pipe(
+    return this.profil$.pipe(
       switchMap(profil => this.profilService.saveNewProfil(profil.id, form.value)),
     ).subscribe();
   }
