722c46969c29910eb487a9b230c137e0a0f9dec2
Louka- <simon.haudegond@gmail.com>
2022-10-12 21:37:29 +0200

    finalizing sorting sales and conditioning titlte for update sale

--- client/src/app/all-sales/all-sales.component.ts
+++ client/src/app/all-sales/all-sales.component.ts
@@ -1,5 +1,6 @@
+import { CreateDateColumn } from 'typeorm';
 import { Component, Injectable, OnInit } from '@angular/core';
-import { map, Observable, switchMap } from 'rxjs';
+import { map, Observable, switchMap, tap } from 'rxjs';
 import { Profil } from '../models/profil.model';
 import { Sale } from '../models/sale.model';
 import { ProfilService } from '../service/profil.service';
@@ -12,12 +13,29 @@ import { SalesService } from '../service/sales.service';
 })
 
 @Injectable()
-export class AllSalesComponent {
+export class AllSalesComponent implements OnInit {
+  profils: Profil[] = [];
 
-  profils$: Observable<Profil[]> = this.profilService.getAllUsersProfil();
+  sales: Sale[] = [];
 
   constructor(
     private profilService: ProfilService,
   ) { }
+
+  ngOnInit(): void {
+    this.profilService.getAllUsersProfil().pipe(
+      map(profils => this.profils = profils),
+      map(profils => profils.forEach(profil => profil.sales.forEach(sale => this.sales.push(sale)))),
+    ).subscribe();
+  }
+
+  geProfilInfos(sale: Sale): Profil | undefined {
+    const profil = this.profils.find(profil => profil.id === sale.profilId)
+    return profil;
+  }
+
+  sortSales(sales: Sale[]): Sale[] {
+    return sales.sort((objA, objB) => new Date(objB.createdAt).getTime() - new Date(objA.createdAt).getTime());
+  }
 
 }
