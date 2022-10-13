21b36808d63638e3ede88ccd197149afba0d9eb2
Louka- <simon.haudegond@gmail.com>
2022-10-13 21:38:41 +0200

    clean dead code

--- src/decorators/user.decorator.ts
+++ src/decorators/user.decorator.ts
@@ -1,6 +1,5 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-// Decorateur récupérable via @User
 export const User = createParamDecorator(
   (data: unknown, ctx: ExecutionContext) => {
     const request = ctx.switchToHttp().getRequest();
--- src/interfaces/payload.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface PayloadInterface {
-  email: string;
-}
