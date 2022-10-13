481b975321eebbd85117b385697a48600c576854
Louka- <simon.haudegond@gmail.com>
2022-10-13 21:40:37 +0200

    finalize usersService

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,12 +1,14 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { Repository, MoreThanOrEqual } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 import { User } from '../entities/user.entity';
-import { LoginCredentialsDto } from '../dtos/login-credentials.dto';
-import { UserSubscribeDto } from 'src/dtos/user-subscribe.dto';
 import { ProfilService } from './profil.service';
+import { DateTime } from "luxon";
+import * as randomToken from 'rand-token';
+import { RegistrationReqModel } from 'src/dtos/registration.req.model';
+import { RegistrationRespModel } from 'src/dtos/registration.resp.model';
 
 @Injectable()
 export class UsersService {
@@ -25,6 +27,10 @@ export class UsersService {
     return this.usersRepository.findOne(id);
   }
 
+  findOne(email: string): Promise<User | undefined> {
+    return this.usersRepository.findOne({ email: email });
+  }
+
   updateUser(user, profil) {
     return this.usersRepository.update(profil, user);
   }
@@ -33,73 +39,136 @@ export class UsersService {
     await this.usersRepository.delete(id);
   }
 
-  async register(userData: UserSubscribeDto): Promise<Partial<User>> {
+  private async registrationValidation(
+    regModel: RegistrationReqModel,
+  ): Promise<string> {
+    if (!regModel.email) {
+      return "Email can't be empty";
+    }
 
-    // if (userData.password !== userData.confirmPassword) {
-    //   try {
-    //     throw new Error("les mots de passe ne sont pas identiques")
-    //   } catch (e) {
-    //     console.log("passwords error");
-    //     console.log(e)
-    //   }
-    // }
+    const emailRule =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    if (!emailRule.test(regModel.email.toLowerCase())) {
+      return 'Invalid email';
+    }
 
-    const today = new Date(Date.now())
+    const user = await this.usersRepository.findOne({ email: regModel.email });
+    if (user != null && user.email) {
+      return 'Email already exist';
+    }
 
-    const user = this.usersRepository.create(userData);
-    user.salt = await bcrypt.genSalt();
-    user.password = await bcrypt.hash(user.password, user.salt);
+    if (regModel.password !== regModel.confirmPassword) {
+      return 'Confirm password not matching';
+    }
+    return '';
+  }
 
+  private async getPasswordHash(password: string): Promise<string> {
+    const hash = await bcrypt.hash(password, 10);
+    return hash;
+  }
+
+  public async registerUser(
+    regModel: RegistrationReqModel,
+  ): Promise<RegistrationRespModel> {
+    let result = new RegistrationRespModel();
+
+    const errorMessage = await this.registrationValidation(regModel);
+    if (errorMessage) {
+      result.message = errorMessage;
+      result.successStatus = false;
+
+      return result;
+    }
+
+    let newUser = new User();
+
+    newUser.email = regModel.email;
+    newUser.password = await this.getPasswordHash(regModel.password);
+
+    const today = DateTime.now()
     const emptyModel = {
       name: '',
       race: 'Ghost',
       description: 'to be filled',
       picture: "",
       birthday: today,
-      phone: '1234567890',
+      phone: '0606060606',
       city: 'NoWhere',
       level: null,
     };
 
     const emptyProfil = await this.profilService.create(emptyModel);
 
-    user.profil = emptyProfil;
-    user.createdAt = today
+    newUser.profil = emptyProfil;
+    newUser.createdAt = today
 
-    await this.usersRepository.save(user);
-    return {
-      id: user.id,
-      email: user.email,
-      profil: user.profil,
-      createdAt: user.createdAt,
-      //TODO: add a role
-    };
+    await this.usersRepository.insert(newUser);
+    result.successStatus = true;
+    result.message = 'success';
+    return result;
   }
 
-  async login(credentials: LoginCredentialsDto) {
+  public async validateUserCredentials(
+    email: string,
+    password: string,
+  ): Promise<User> {
+    let user = await this.usersRepository.findOne({ email: email });
 
-    const { email, password } = credentials;
-    const user = await this.usersRepository.createQueryBuilder("user")
-      .where("user.email = :email",
-        { email }
-      )
-      .getOne();
-
-    if (!user)
-      throw new NotFoundException('email ou password erronée');
-    const hashedPassword = await bcrypt.hash(password, user.salt);
-    if (hashedPassword === user.password) {
-      const payload = {
-        email: user.email,
-      };
-      const jwt = await this.jwtService.sign(payload);
-      return {
-        "id": user.id,
-        "email": user.email,
-        "jwt": jwt,
-      };
-    } else {
-      throw new NotFoundException('email ou password erronée');
+    if (user == null) {
+      return null;
     }
+
+    const isValidPassword = await bcrypt.compare(password, user.password);
+    if (!isValidPassword) {
+      return null;
+    }
+
+    let currentUser = new User();
+    currentUser.id = user.id;
+    currentUser.email = user.email;
+
+    return currentUser;
+  }
+
+  public async getJwtToken(user: User): Promise<string> {
+    const payload = {
+      ...user,
+    };
+    return this.jwtService.signAsync(payload);
+  }
+
+  public async getRefreshToken(userId: number): Promise<string> {
+    const userDataToUpdate = {
+      refreshToken: randomToken.generate(16),
+      refreshTokenExp: DateTime.now().set({ days: 14 }).toFormat('YYYY/MM/DD'),
+    };
+
+    await this.usersRepository.update(userId, userDataToUpdate);
+    return userDataToUpdate.refreshToken;
+  }
+
+  public async validRefreshToken(
+    email: string,
+    refreshToken: string,
+  ): Promise<User> {
+    const currentDate = DateTime.now().set({ days: 14 }).toFormat('YYYY/MM/DD');
+    let user = await this.usersRepository.findOne({
+      where: {
+        email: email,
+        refreshToken: refreshToken,
+        refreshTokenExp: MoreThanOrEqual(currentDate),
+      },
+    });
+
+    if (!user) {
+      return null;
+    }
+
+    let currentUser = new User();
+    currentUser.id = user.id;
+    currentUser.email = user.email;
+
+    return currentUser;
   }
 }
